7b484955bd6b1366fb5df04176e62b461dab4aee
josh <itzdi0de@gmail.com>
2021-03-10 07:12:58 -0500

    v2 babyyyy
    Complete overhaul. Not where I would like it to be but less threads and hopefully easier to maintain going forward.

---
+++ dewcon.py
@@ -0,0 +1,17 @@
+import re
+
+class Dewparser(object):
+    def __init__(self, message):
+        self.message = message
+
+    def parse(self):
+
+        self.srv_msg = self.message.split(" ")
+        self.player_data = re.split('[ /<>]', self.srv_msg[2])
+
+        self.date = self.srv_msg[0].strip("[")
+        self.time = self.srv_msg[1].strip("]")
+        self.name = self.player_data[1]
+        self.uid = self.player_data[2]
+        self.ip = self.player_data[3]
+        self.message = self.srv_msg[3:]
